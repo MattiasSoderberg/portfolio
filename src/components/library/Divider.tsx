7d20d0c2b212ac45b3758fcaaa3550eb50ce509e
Mattias Söderberg <mattes.email@gmail.com>
2023-07-12 16:09:46 +0200

    Add border radius

--- src/components/library/Divider.tsx
+++ src/components/library/Divider.tsx
@@ -8,9 +8,10 @@ interface Props {
 const Divider = ({ horizontal = false, wide = true }: Props) => {
   const horizontalSize = wide ? "w-full h-[6px]" : "w-full h-[2px]";
   const verticalSize = wide ? "w-[6px] h-full" : "w-[2px] h-full";
+
   const classes = horizontal
     ? `divider-linear-gradient-horizontal ${horizontalSize}`
-    : `divider-linear-gradient-vertical self-center ${verticalSize}`;
+    : `divider-linear-gradient-vertical self-center ${verticalSize} rounded-s-lg`;
   return <div className={classes} />;
 };
 
