1e542448c634bb6e784499c40177938e2d98bbeb
Mattias Söderberg <mattes.email@gmail.com>
2023-08-30 13:23:56 +0200

    Make container relative

--- src/components/library/Containers/ContentContainer.tsx
+++ src/components/library/Containers/ContentContainer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ContentContainer = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="w-full h-full max-w-screen-3xl max-h-screen-3xl flex flex-col justify-center items-center px-40 pt-14 pb-24">
+    <div className="w-full h-full max-w-screen-3xl max-h-screen-3xl flex flex-col justify-center items-center px-40 pt-14 pb-24 relative">
       {children}
     </div>
   );
