717990a7d7c89532d7c06288379051104468d578
Mattias Söderberg <mattes.email@gmail.com>
2023-08-31 16:17:16 +0200

    Style fix

--- src/components/library/LandingFooter.tsx
+++ src/components/library/LandingFooter.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Image from "next/image";
 import { SanityDocument } from "next-sanity";
-import imageUrlBuilder from "@sanity/image-url";
 import getImageUrl from "@/utils/getImageUrl";
 import { TextRegular } from "./Typography";
 import ButtonStandard from "./Button/variants/ButtonStandard";
@@ -20,8 +19,8 @@ const LandingFooter = ({ document }: Props) => {
   };
 
   return (
-    <div className="w-full md:max-w-[90%] xl:max-w-[80%] h-fit flex flex-col items-center sm:items-start sm:flex-row gap-5 lg:gap-10 2xl:gap-20">
-      <div className="w-fit h-fit min-w-[150px] 2xl:min-w-[200px] min-h-[150px] 2xl:min-h-[200px] relative rounded-full overflow-hidden filter grayscale">
+    <div className="w-full h-fit flex flex-col items-center gap-4 sm:items-start sm:flex-row md:max-w-[90%] lg:gap-8 xl:max-w-[80%] 2xl:gap-16">
+      <div className="w-fit h-fit min-w-[150px] min-h-[150px] relative rounded-full overflow-hidden filter grayscale 2xl:min-w-[200px] 2xl:min-h-[200px]">
         <Image
           src={getImageUrl(landingFooter?.image)}
           alt={landingFooter?.image.alt || "No alt text"}
@@ -29,7 +28,7 @@ const LandingFooter = ({ document }: Props) => {
           sizes="(max-width: 200px) 200px"
         />
       </div>
-      <div className="flex flex-col items-center sm:items-end justify-end gap-5 xl:gap-10">
+      <div className="w-full h-full flex flex-col items-center justify-end gap-5 sm:items-end xl:gap-10">
         <TextRegular color="firstLighter">{landingFooter?.text}</TextRegular>
         <div className="p-0 sm:pr-10">
           <ButtonStandard
