9995f8b77c4dca926b05a7830cdb7856ee5b7fed
Mattias Söderberg <mattes.email@gmail.com>
2023-08-31 16:17:43 +0200

    Change padding

--- src/components/library/Containers/ContentContainer.tsx
+++ src/components/library/Containers/ContentContainer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ContentContainer = ({ children }: { children: React.ReactNode }) => {
   return (
-    <section className="w-full h-full max-w-screen-3xl max-h-screen-3xl flex flex-col justify-center items-center px-2 md:px-10 lg:px-20 2xl:px-40 pt-0 md:pt-14 pb-2 md:pb-24 relative">
+    <section className="w-full h-full max-w-screen-3xl max-h-screen-3xl flex flex-col justify-center items-center px-2 md:px-10 lg:px-20 2xl:px-40 pt-2 md:pt-14 pb-2 md:pb-28 relative">
       {children}
     </section>
   );
