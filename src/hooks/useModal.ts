541c5f0ca8ff8183910c5ae8cb8dbdde555c59e2
Mattias Söderberg <mattes.email@gmail.com>
2023-09-03 19:04:37 +0200

    Remove unused code

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -4,8 +4,7 @@ import { useModalContext } from "@/context/ModalContext";
 type ContentType = "contact" | "projects" | "none";
 
 const useModal = () => {
-  // const { handleOnOpen, handleOnClose } = useModalContext()
-  const modalContext = useModalContext()
+  const modalContext = useModalContext();
 
   const openModal = (content: ContentType) => {
     modalContext?.handleOnOpen(content);
@@ -14,8 +13,6 @@ const useModal = () => {
   const closeModal = () => {
     modalContext?.handleOnClose();
   };
-
-
 
   return { openModal, closeModal };
 };
