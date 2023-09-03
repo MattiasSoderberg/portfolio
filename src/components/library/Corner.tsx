9a0d9f10ed780990c165c312247cc362e8bed49d
Mattias Söderberg <mattes.email@gmail.com>
2023-09-03 19:01:14 +0200

    Refactor style to change on page change

--- src/components/library/Corner.tsx
+++ src/components/library/Corner.tsx
@@ -9,13 +9,15 @@ export const POSITIONS = {
 } as const;
 
 type Position = (typeof POSITIONS)[keyof typeof POSITIONS];
+type Color = "landing" | "projects";
 
 interface Props {
   position: Position;
   small?: boolean;
+  color?: Color;
 }
 
-const Corner = ({ position, small = false }: Props) => {
+const Corner = ({ position, small = false, color = "landing" }: Props) => {
   const positionClasses = {
     topLeft: "top-0 left-0",
     topRight: "rotate-90 top-0 right-0",
@@ -24,6 +26,9 @@ const Corner = ({ position, small = false }: Props) => {
   };
 
   const cornerClasses = `absolute ${positionClasses[position]}`;
+  const styleClasses = `${
+    color == "landing" ? "bg-firstMain" : "bg-secondMain"
+  } transition-colors duration-500 ease-out rounded-s-lg overflow-hidden`;
 
   const variants = {
     initial: { borderRadius: "0px" },
@@ -37,16 +42,45 @@ const Corner = ({ position, small = false }: Props) => {
     <div className={cornerClasses}>
       <motion.div
         variants={variants}
-        // transition={{ duration: 0.5 }}
-        className="w-[150px] sm:w-[250px] h-[5px] lg:h-[8px] xl:h-[10px] corner-linear-gradient-horizontal z-0"
-      />
+        className={`w-[150px] xl:w-[250px] h-[5px] xl:h-[8px] 2xl:h-[10px] ${styleClasses}`}
+      >
+        <div className="w-full h-full corner-linear-gradient-horizontal-overlay" />
+      </motion.div>
       <motion.div
         variants={variants}
-        // transition={{ duration: 0.5 }}
-        className="w-[5px] lg:w-[8px] xl:w-[10px] h-[145px] lg:h-[242px] xl:h-[240px] corner-linear-gradient-vertical z-0"
-      />
+        className={`w-[5px] xl:w-[8px] 2xl:w-[10px] h-[145px] xl:h-[242px] 2xl:h-[240px] ${styleClasses}`}
+      >
+        <div className="w-full h-full corner-linear-gradient-vertical-overlay" />
+      </motion.div>
     </div>
   );
+  // color == "landing" ? (
+  //   <div className={cornerClasses}>
+  //     <motion.div
+  //       variants={variants}
+  //       // transition={{ duration: 0.5 }}
+  //       className="w-[150px] sm:w-[250px] h-[5px] lg:h-[8px] xl:h-[10px] corner-linear-gradient-horizontal z-0 transition-colors duration-150 ease-in"
+  //     />
+  //     <motion.div
+  //       variants={variants}
+  //       // transition={{ duration: 0.5 }}
+  //       className="w-[5px] lg:w-[8px] xl:w-[10px] h-[145px] lg:h-[242px] xl:h-[240px] corner-linear-gradient-vertical z-0 transition-colors duration-150 ease-in"
+  //     />
+  //   </div>
+  // ) : (
+  //   <div className={cornerClasses}>
+  //     <motion.div
+  //       variants={variants}
+  //       // transition={{ duration: 0.5 }}
+  //       className="w-[150px] sm:w-[250px] h-[5px] lg:h-[8px] xl:h-[10px] corner-linear-gradient-horizontal-green z-0 transition-colors duration-150 ease-in"
+  //     />
+  //     <motion.div
+  //       variants={variants}
+  //       // transition={{ duration: 0.5 }}
+  //       className="w-[5px] lg:w-[8px] xl:w-[10px] h-[145px] lg:h-[242px] xl:h-[240px] corner-linear-gradient-vertical-green z-0 transition-colors duration-150 ease-in"
+  //     />
+  //   </div>
+  // );
 };
 
 export default Corner;
