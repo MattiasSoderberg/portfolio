5803c3355bd51124486fd1e7064654cb74207b87
Mattias Söderberg <mattes.email@gmail.com>
2024-01-17 14:56:10 +0100

    Add enter and exit animation on error message

--- src/components/library/Form/WrapperWErrorMsg.tsx
+++ src/components/library/Form/WrapperWErrorMsg.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ValidationError } from "yup";
 import { TextRegular, TextSmall } from "../Typography";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface Props {
   children: React.ReactNode;
@@ -15,13 +16,25 @@ const WrapperWErrorMsg = ({
   errors,
   paddingBottom = "7",
 }: Props) => {
-  const error = errors.find((error) => error?.path === name);
+  const error = errors.find((error) => error?.path === name) || null;
+
   return (
     <div className={`w-full pb-${paddingBottom} relative`}>
       {children}
-      <div className="absolute left-0 bottom-0">
-        <TextRegular color="thirdLight">{error?.message}</TextRegular>
-      </div>
+      <AnimatePresence>
+        {error && (
+          <motion.div
+            key={name}
+            className="absolute left-0 bottom-0"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.1 }}
+            exit={{ opacity: 0 }}
+          >
+            <TextRegular color="thirdLight">{error?.message}</TextRegular>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
