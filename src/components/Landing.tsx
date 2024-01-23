bf7e87c4f43a8dbcceba8aee4a507697f273ffd3
Mattias Söderberg <mattes.email@gmail.com>
2024-01-23 13:40:34 +0100

    Remove unused code

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import { H1, H2, TextLarge } from "./library/Typography";
 import Divider from "./library/Divider";
