c90aabcc718eeaa6e01c3f2c9a74d7f4876430e2
Mattias Söderberg <mattes.email@gmail.com>
2024-01-23 08:14:10 +0100

    Add one project query

--- sanity/lib/query.ts
+++ sanity/lib/query.ts
@@ -2,3 +2,4 @@ import { groq } from "next-sanity";
 
 export const projectsQuery = groq`*[_type == "project"]`;
 export const landingContentQuery = groq`*[_type == "landingContent"]`;
+export const oneProjectQuery = groq`*[_type == "project" && slug.current == $slug][0]`;
