0d2e1b64a3dd5bbb84f064ab40629add7aef0a50
Mattias Söderberg <mattes.email@gmail.com>
2024-01-23 16:00:15 +0100

    Only fetching the first element in landing content

--- sanity/lib/query.ts
+++ sanity/lib/query.ts
@@ -1,5 +1,5 @@
 import { groq } from "next-sanity";
 
 export const projectsQuery = groq`*[_type == "project"]`;
-export const landingContentQuery = groq`*[_type == "landingContent"]`;
+export const landingContentQuery = groq`*[_type == "landingContent"][0]`;
 export const oneProjectQuery = groq`*[_type == "project" && slug.current == $slug][0]`;
